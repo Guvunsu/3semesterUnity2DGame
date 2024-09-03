c5c2b55ea27addcc62ce80a8219a9c778bd1af60
Guvunsu <gavryk_fray_rojas@hotmail.com>
2024-09-02 23:10:55 -0600

    documentacion del script UiMaganer

--- UnityGameIn2D/Assets/scripts/UIManager.cs
+++ UnityGameIn2D/Assets/scripts/UIManager.cs
@@ -12,12 +12,16 @@ public class UIManager : MonoBehaviour
     [SerializeField] List<GameObject> m_listOfSign;
 
     UIState m_currentState;
+
     int m_newPosition;
 
     #region Funciones Publicas
 
     /// <summary>
-    /// cambia el estado del UIManager
+    /// cambia el estado del UIManager, FMS donde se dedicara a activar y desactivar la adeventencia con un switch y condicional.
+    /// Con el nombre de mi variable de ese llamado de libreria de ENUM y otra variable, si~ es verdaramente igual se regresara a su estado original.
+    /// con un switch se ejecutara la funcion TimeToTurnOffSign() y finalizara hasta llegar desactivateNewSign()
+    /// para activar el temporizador de la señal y se descativara depsues de que pase el tiempo 
     /// </summary>
     /// <param name="p_newState"></param>
 
@@ -29,7 +33,7 @@ public class UIManager : MonoBehaviour
         }
 
         m_currentState = p_newState;
-
+        
         switch (m_currentState)
         {
             case UIState.None:
@@ -43,7 +47,11 @@ public class UIManager : MonoBehaviour
         }
 
     }
-
+    /// <summary>
+    /// nombraremos esta funcion para colocar la advertencia de manera random y nombrandola con un variable tipo int 
+    /// para asignar uno de nuestras opciones numeradas poscisionadas (3)
+    /// </summary>
+    /// <param name="p_newSign"></param>
     public void setNewRandomPosition(int p_newSign)
     {
         m_newPosition = p_newSign;
@@ -52,6 +60,9 @@ public class UIManager : MonoBehaviour
 
 
     #region Funciones Privadas
+    /// <summary>
+    /// si nuestra instancia es verdaderamente algun objeto es referencia en Unity se llama esra, si no, se destruye a si misma
+    /// </summary>
     void Awake()
     {
         if (instance == null)
@@ -63,12 +74,20 @@ public class UIManager : MonoBehaviour
             Destroy(instance);
         }
     }
-
+    /// <summary>
+    /// hacemas una funcion para activar la ueva señal en cada inicio de de nueva partida 
+    /// ocupando una lirberia de Gameobject se le asignara un lugar random gracias a la funcion setNewRandomPosition y se le activa
+    /// empieza nuestra corrutina que activara la funcion de tiempo de espera TimeToTurnOffSign y a su vez setNewRandomPosition()
+    /// </summary>
     void activateNewSign()
     {
         m_listOfSign[m_newPosition].SetActive(true);
         StartCoroutine(TimeToTurnOffSign(m_newPosition));
     }
+    /// <summary>
+    /// se llama la libreria de GameObject m_listOfSign para luego llamar la funcion de setNewRandomPosition y desactivarlo
+    /// y ya no estara la advertencia
+    /// </summary>
     void desactivateNewSign()
     {
         m_listOfSign[m_newPosition].SetActive(false);
@@ -76,7 +95,15 @@ public class UIManager : MonoBehaviour
 
     #endregion
 
-  
+
+    /// <summary>
+    /// llamamos la libreria Enum llamado TimeToTurnOffSign con una variable tipo entero int 
+    /// la corutina se parara y desctivara la advertencia depsues de una esclara de tiempo de 3 segundos 
+    /// despues de verificar el gameObject asignado random de nuestras opciones par la advertencia y lo desactivara
+    /// se pasara a la funcion changeUIManagerState y se verificara con una condicional y un switch el cambio de estados por la lista enum
+    /// </summary>
+    /// <param name="p_newSign"></param>
+    /// <returns></returns>
     IEnumerator TimeToTurnOffSign(int p_newSign)
     {// cuando tengamos activatodo el activatenewsign le podemos poner que spawne los enemigos cada cierto segundos 
         yield return new WaitForSeconds(3);
@@ -85,7 +112,9 @@ public class UIManager : MonoBehaviour
 
     }
 }
-
+/// <summary>
+/// llamamos una lista llamada enum  de una libreria UIState que se dedicara a llamar la funcion changeUIManagerState
+/// </summary>
 public enum UIState
 {
     None,
